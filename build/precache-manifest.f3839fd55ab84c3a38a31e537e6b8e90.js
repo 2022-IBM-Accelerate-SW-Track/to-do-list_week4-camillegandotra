a454606b72f848a16ff55414b68cb864bf2255f7
Camille <cgandotr@ucsc.edu>
2022-07-01 13:34:35 -0700

    week4

---
+++ build/precache-manifest.f3839fd55ab84c3a38a31e537e6b8e90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bd5afdd4ad708b51429",
+    "url": "/static/css/main.39a44d97.chunk.css"
+  },
+  {
+    "revision": "4bd5afdd4ad708b51429",
+    "url": "/static/js/main.4bd5afdd.chunk.js"
+  },
+  {
+    "revision": "e8bb4d5c7a6b23fd434e",
+    "url": "/static/css/1.865ba6d7.chunk.css"
+  },
+  {
+    "revision": "e8bb4d5c7a6b23fd434e",
+    "url": "/static/js/1.e8bb4d5c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "78bb3e43513587f2f9700b5d619db7b7",
+    "url": "/static/media/profile_pic.78bb3e43.jpg"
+  },
+  {
+    "revision": "ec23d15021979a686562f523992dd022",
+    "url": "/index.html"
+  }
+];
